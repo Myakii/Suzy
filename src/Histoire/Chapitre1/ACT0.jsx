7fc983bc3bbbfbfa836a53de11c1cd51af83aaad
Myaki <lili_yang@hotmail.fr>
2024-03-29 23:43:25 +0100

    add bgm and profil image

--- src/Histoire/Chapitre1/ACT0.jsx
+++ src/Histoire/Chapitre1/ACT0.jsx
@@ -2,8 +2,10 @@ import React, { useState, useEffect } from "react";
 
 import "../../css/Chapitre1css/ACT0.css";
 import Dialogue from "../../components/Dialogue";
+// Assets
 import Alarme from "../../Assets/Sons/alarm_clock.mp3";
 import Bgm_Chambre_Suzy from '../../Assets/Musiques/Golden_Eye.mp3';
+import Bg_Chambre_Suzy from '../../Assets/Fonds/bg_chambre_suzy.jpg'
 
 const ACT0 = () => {
   const dialogues = [
@@ -22,20 +24,24 @@ const ACT0 = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [isPlaying, setIsPlaying] = useState(false);
   const [isBgmPlaying, setIsBgmPlaying] = useState(false);
-
-  const bgmVolume = 0.1
+  const [isBg, setIsBg] = useState(false);
+  const [isProfil, setIsProfil] = useState(false);
 
   const handleClick = () => {
     if (currentIndex < dialogues.length - 1) {
       setCurrentIndex(currentIndex + 1);
       if (currentIndex === 5) {
         setIsPlaying(false); // Arrêter le son avant d'afficher le troisième dialogue
+        setIsBg(true); // Faire apparaitre le background de la chambre
+        setIsProfil(true); // Faire apparaitre le profil
       }
       if (currentIndex === 2) {
         setIsPlaying(true); // Démarrer le son avant d'afficher le troisième dialogue
       }
     } else {
       setIsPlaying(false);
+      setIsBg(false)
+      setIsProfil(false)
     }
   };
 
@@ -55,7 +61,13 @@ const ACT0 = () => {
         <audio src={Alarme} autoPlay loop />
       )}
       {isBgmPlaying && (
-        <audio src={Bgm_Chambre_Suzy} autoPlay loop onVolumeChange={bgmVolume}/>)}
+        <audio src={Bgm_Chambre_Suzy} autoPlay loop />)}
+      {isBg && (
+        <div className="Bg_Chambre"></div>
+      )}
+      {isProfil && (
+        <div className="Suzy_Pyjama_Neutre"></div>
+      )}
     </div>
   );
 };
