36feddd46c8a12874b7803ebf519886fc9f2be3e
Myaki <lili_yang@hotmail.fr>
2024-04-14 21:37:13 +0200

    icon porte

--- src/Histoire/Chapitre1/ACT0.jsx
+++ src/Histoire/Chapitre1/ACT0.jsx
@@ -6,17 +6,17 @@ import Dialogue from "../../components/Dialogue";
 
 // Assets
 import Alarme from "../../Assets/Sons/alarm_clock.mp3";
-import EteintAlarme from '../../Assets/Sons/light-switch.mp3'
-import BougerLit from '../../Assets/Sons/sitting-on-bed-97752.mp3'
+import EteintAlarme from "../../Assets/Sons/light-switch.mp3";
+import BougerLit from "../../Assets/Sons/sitting-on-bed-97752.mp3";
 
-
-import Bgm_Chambre_Suzy from '../../Assets/Musiques/Golden_Eye.mp3';
+import Bgm_Chambre_Suzy from "../../Assets/Musiques/Golden_Eye.mp3";
 
 const ACT0 = () => {
   const dialogues = [
     {
       nom: "???",
-      texte: " Tu sais que tout ira bien, bien qu'on ne soit plus que deux, n'est-ce pas ?",
+      texte:
+        " Tu sais que tout ira bien, bien qu'on ne soit plus que deux, n'est-ce pas ?",
     },
     { nom: "???", texte: "..." },
     { nom: "???", texte: "Je t'aimerai toute ma vie, ma pe..." },
@@ -24,21 +24,25 @@ const ACT0 = () => {
     { nom: "???", texte: "Hmm...Il est déjà 7h...?" },
     { nom: "???", texte: "Arg..." },
     { nom: "???", texte: "..." },
-    { nom: "???", texte: "Je devrais me préparer et aller déjeuner." }
+    { nom: "???", texte: "Je devrais me préparer et aller déjeuner." },
   ];
 
-  const interactions = [
+  const interactionsPorteSuzy = [
     { nom: "???", texte: " Je devrais d'abord m'habiller." },
-    { nom: "???", texte: "..." }
   ];
 
   const interactionsArmoire = [
-    { nom: "???", texte: "..." },
-    { nom: "???", texte: "Bien, je suis prête ! C'est étrange, aujourd'hui, j'ai vraiment envie d'aller à l'école." },
     {
-      nom: "???", texte: "Ce n'est pas tous les jours qu'on a ce genre de sentiment. Peut-être que quelque chose de bien va se produire ?"
+      nom: "???",
+      texte:
+        "Bien, je suis prête ! C'est étrange, aujourd'hui, j'ai vraiment envie d'aller à l'école.",
     },
-  ]
+    {
+      nom: "???",
+      texte:
+        "Ce n'est pas tous les jours qu'on a ce genre de sentiment. Peut-être que quelque chose de bien va se produire ?",
+    },
+  ];
 
   const [currentIndex, setCurrentIndex] = useState(0);
   const [interactionsIndex, setInteractionsIndex] = useState(0);
@@ -64,14 +68,14 @@ const ACT0 = () => {
   const [isIconArmoire, setIsIconArmoire] = useState(false);
   const [isIconPorteChambreSuzy, setIsIconPorteChambreSuzy] = useState(false);
   const [isShowDialogueArmoire, setIsShowDialogueArmoire] = useState(false);
+  const [isShowDialoguePorte, setIsShowDialoguePorte] = useState(false);
 
   const [isBgBackNonCinematique, setIsBgBlackNonCinematique] = useState(false);
 
   // TEMPS
-  const delay = ms => new Promise(res => setTimeout(res, ms));
+  const delay = (ms) => new Promise((res) => setTimeout(res, ms));
 
   const habiller = false;
-
 
   const handleClick = () => {
     if (currentIndex < dialogues.length - 1) {
@@ -85,11 +89,11 @@ const ACT0 = () => {
 
       if (currentIndex === 5) {
         setIsPlaying(false); // Arrêter le son du réveil
-        setIsBgmEteindreAlarme(true) // Mettre le son du bouton éteindre
+        setIsBgmEteindreAlarme(true); // Mettre le son du bouton éteindre
       }
 
       if (currentIndex === 4) {
-        setIsBgmBougerLit(true)
+        setIsBgmBougerLit(true);
       }
 
       if (currentIndex === 2) {
@@ -100,7 +104,7 @@ const ACT0 = () => {
         setDisplayText(true); // Apparaitre le texte
       }
     } else {
-      // Comme un : 
+      // Comme un :
       // if (currentIndex === 7) {
       setIsProfilSuzyCinematique(false);
       setIsIconArmoire(true);
@@ -113,26 +117,31 @@ const ACT0 = () => {
   };
 
   const handleClickInteraction = () => {
+    setInteractionsIndex(interactionsIndex + 1);
+    setIsBgChambre(true);
+  };
+
+  const handleClickInteraction2 = () => {
     setInteractionsIndex2(interactionsIndex2 + 1);
     setIsBgChambre(true);
   };
 
-  const handleShowDialogueGamePlay = (habiller) => {
-
+  const handleClickIconPorte = async () => {
     // Si les conditons ne sont pas remplies, affiche le dialogue Porte Chambre de Suzy
+    habiller = await handleClickIconArmoire(habiller);
     if (habiller === true) {
       setIsShowDialogueArmoire(false);
       setIsProfilSuzyPyjama(false);
       setIsBgBack(true);
+      // A AJOUTER -> SINON SORTIE DE LA CHAMBRE
     } else {
-      setIsShowDialogueArmoire(true);
-      setIsProfilSuzyEcole(true);
+      setIsShowDialoguePorte(true);
+      setIsProfilSuzyPyjama(true);
     }
-    // A AJOUTER -> SINON SORTIE DE LA CHAMBRE
   };
 
-
-  const handleClickIconArmoire = async () => {
+  const handleClickIconArmoire = async (habiller) => {
+    habiller = true;
     setIsBgChambre(false);
     setIsIconArmoire(false);
     setIsIconPorteChambreSuzy(false);
@@ -143,17 +152,32 @@ const ACT0 = () => {
     setIsBgChambre(true);
     setIsShowDialogueArmoire(true);
     setIsProfilSuzyEcole(true);
+    return habiller;
   };
 
-  // Après avoir défini le statut du dialogue, vérifie si c'est le dernier dialogue
+  // Après avoir défini le statut du dialogue, vérifie si c'est le dernier dialogue armoire
   useEffect(() => {
-    if (isShowDialogueArmoire && interactionsIndex2 === interactionsArmoire.length - 1) {
+    if (
+      isShowDialogueArmoire &&
+      interactionsIndex2 === interactionsArmoire.length - 1
+    ) {
       setIsShowDialogueArmoire(false);
       setIsProfilSuzyEcole(false);
       setIsIconPorteChambreSuzy(true);
     }
   }, [isShowDialogueArmoire, interactionsIndex2, interactionsArmoire.length]);
 
+  // Après avoir défini le statut du dialogue, vérifie si c'est le dernier dialogue porte
+  useEffect(() => {
+    if (
+      isShowDialoguePorte &&
+      interactionsIndex === interactionsPorteSuzy.length - 1
+    ) {
+      setIsShowDialoguePorte(true);
+      setIsProfilSuzyPyjama(true);
+      setIsIconPorteChambreSuzy(false);
+    }
+  }, [isShowDialoguePorte, interactionsIndex, interactionsPorteSuzy.length]);
 
   // Fait apparaitre immédiatement les éléments dès le chargement
   useEffect(() => {
@@ -165,6 +189,7 @@ const ACT0 = () => {
 
   return (
     <div id="chapitre1">
+      {/* CINEMATIQUE 1 */}
       {/* Assure que dialogues[currentIndex] est défini avant d'essayer d'accéder à sa propriété 'nom'. */}
       {isDisplayText && (
         <Dialogue nom={dialogues[currentIndex].nom} onClick={handleClick}>
@@ -173,29 +198,25 @@ const ACT0 = () => {
       )}
 
       {/* Audios */}
-      {isBgmEteindreAlarme && (
-        <audio src={EteintAlarme} autoPlay />
-      )}
+      {isBgmEteindreAlarme && <audio src={EteintAlarme} autoPlay />}
 
-      {isBgmBougerLit && (
-        <audio src={BougerLit} autoPlay />
-      )}
+      {isBgmBougerLit && <audio src={BougerLit} autoPlay />}
 
-      {isPlaying && (
-        <audio src={Alarme} autoPlay loop />
-      )}
+      {isPlaying && <audio src={Alarme} autoPlay loop />}
 
       {isBgmPlaying && (
-        <audio src={Bgm_Chambre_Suzy} className="Bgm_Chambre_Suzy" autoPlay loop controls />
+        <audio
+          src={Bgm_Chambre_Suzy}
+          className="Bgm_Chambre_Suzy"
+          autoPlay
+          loop
+          controls
+        />
       )}
 
       {/* Backgrounds */}
-      {isBgChambre && (
-        <div className="Bg_Chambre_Suzy"></div>
-      )}
-      {isBgBack && (
-        <div className="Bg_Black"></div>
-      )}
+      {isBgChambre && <div className="Bg_Chambre_Suzy"></div>}
+      {isBgBack && <div className="Bg_Black"></div>}
 
       {/* Profils */}
       {isProfilSuzyCinematique && (
@@ -206,7 +227,9 @@ const ACT0 = () => {
         <div className="Suzy_Pyjama_Neutre_Gameplay"></div>
       )}
 
-      {/* Mode de jeu */}
+      {/* FIN CINEMATIQUE 1 */}
+
+      {/* Mode de jeu - ARMOIRE */}
       {isIconArmoire && (
         <div className="Icon_Armoire" onClick={handleClickIconArmoire}>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
@@ -227,18 +250,23 @@ const ACT0 = () => {
       )}
 
       {isShowDialogueArmoire && (
-        <Dialogue nom={interactionsArmoire[interactionsIndex2].nom}
-          onClick={handleClickInteraction}>
+        <Dialogue
+          nom={interactionsArmoire[interactionsIndex2].nom}
+          onClick={handleClickInteraction2}
+        >
           {interactionsArmoire[interactionsIndex2].texte}
         </Dialogue>
       )}
 
-      {isProfilSuzyEcole && (
-        <div className="Suzy_Ecole_Neutre"></div>
-      )}
+      {isProfilSuzyEcole && <div className="Suzy_Ecole_Neutre"></div>}
+      {/* FIN Mode de jeu - ARMOIRE */}
 
-      {isIconPorteChambreSuzy && isBgChambre && (
-        <div className="Icon_Porte_Chambre_Suzy" onClick={handleShowDialogueGamePlay}>
+      {/* Mode de jeu - PORTE */}
+      {isIconPorteChambreSuzy && (
+        <div
+          className="Icon_Porte_Chambre_Suzy"
+          onClick={handleClickIconPorte}
+        >
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
             <path
               fill="#c00000"
@@ -247,15 +275,15 @@ const ACT0 = () => {
               d="M384 192c0 87.4-117 243-168.3 307.2c-12.3 15.3-35.1 15.3-47.4 0C117 435 0 279.4 0 192C0 86 86 0 192 0S384 86 384 192z"
             />
           </svg>
-          {/* {isShowDialogueGamePlay && (
-          <Dialogue nom={interactions[interactionsIndex].nom} onClick={handleClickInteraction}>
-            {interactions[interactionsIndex].texte}
-          </Dialogue>
-        )} */}
         </div>
       )}
+      {isShowDialoguePorte && (
+        <Dialogue nom={interactionsPorteSuzy[interactionsIndex].nom} onClick={handleClickInteraction}>
+          {interactionsPorteSuzy[interactionsIndex].texte}
+        </Dialogue>
+      )}
+      {isBgBack && <div className="BgBlack"></div>}
     </div>
-  );
-};
-
-export default ACT0;
+  )
+}
+export default ACT0
